cd5505c028006c21d389ec15dee3776cec0f7002
dongchao <dongchao@xiaomi.com>
2015-04-03 15:48:15 +0900

    change config name

---
+++ sched_config.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    mysql_connect : {
+        // mysql setting
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "Microblog",
+        charset: "UTF8MB4"
+    },
+    logger_file: "./sched.log",
+    logger_exceptions_file: "./sched_exceptions.log",
+    mail_user: "1193235126@qq.com",
+    mail_pass: "Tju6qdiape"
+
+};
